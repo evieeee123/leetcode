82c97d96f1aeb03dfc5585cffdbff917205c7c09
Evie Zeng <93158200+evieeee123@users.noreply.github.com>
2022-08-16 11:40:06 -0700

    Time: 234 ms (11.18%), Space: 45.6 MB (50.19%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
@@ -2,10 +2,27 @@
  * @param {string} s
  * @return {number}
  */
+// var firstUniqChar = function(s) {
+//     for (let i = 0; i < s.length; i++) {
+//         if (s.indexOf(s[i]) === s.lastIndexOf(s[i])){
+//             return i;
+//         }
+//     }
+//     return -1;
+// };
+
 var firstUniqChar = function(s) {
-    for (let i = 0; i < s.length; i++) {
-        if (s.indexOf(s[i]) === s.lastIndexOf(s[i])){
-            return i;
+    let hash = {};
+    for (let i = 0; i < s.length; i++){
+        if (!hash[s[i]]){
+            hash[s[i]] = 1;
+        }else{
+            hash[s[i]]++;
+        }
+    }
+    for(let i = 0; i < s.length; i++){
+        if (hash[s[i]] === 1){
+            return i
         }
     }
     return -1;
