1f1dc4fa575af8a3aae0f1093d7fe73c0fc50cc6
Evie Zeng <93158200+evieeee123@users.noreply.github.com>
2022-08-18 11:13:20 -0700

    Time: 105 ms (35.82%), Space: 45.1 MB (45.63%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -3,18 +3,36 @@
  * @param {number} target
  * @return {number}
  */
+// var search = function(nums, target) {
+    
+//     let last = nums.length - 1; //5
+//     let start = 0; //4
+
+//     while(start <= last){
+//         let mid = Math.floor((start+last)/2); //4
+//         if (nums[mid] === target) {
+//             return mid;
+//         }else if(nums[mid] < target) {
+//             start = mid + 1;
+//         } else if(nums[mid] > target) {
+//             last = mid - 1;
+//         }
+//     }
+//     return -1;
+// };
+
 var search = function(nums, target) {
     
-    let last = nums.length - 1; //5
-    let start = 0; //4
-
-    while(start <= last){
-        let mid = Math.floor((start+last)/2); //4
-        if (nums[mid] === target) {
+    let first = 0;
+    let last = nums.length - 1;
+    
+    while (first <= last) {
+        let mid = Math.floor((first + last) / 2);
+        if (nums[mid] === target ){
             return mid;
-        }else if(nums[mid] < target) {
-            start = mid + 1;
-        } else if(nums[mid] > target) {
+        } else if (nums[mid] < target){
+            first = mid + 1;
+        } else if (nums[mid] > target){
             last = mid - 1;
         }
     }
