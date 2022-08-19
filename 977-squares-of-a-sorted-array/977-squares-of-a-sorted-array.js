3da922c5453ed100944cf9839566c697212f5b40
Evie Zeng <93158200+evieeee123@users.noreply.github.com>
2022-08-19 15:27:04 -0700

    Time: 188 ms (25.30%), Space: 49.2 MB (17.44%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -2,20 +2,37 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+// var sortedSquares = function(nums) {
+//     for (let i = 0;  i < nums.length; i++) {
+//         nums[i] = nums[i] * nums[i];
+//     }
+    
+//     let first = 0;
+//     let last = nums.length - 1;
+//     let res = [];
+//     while (first <= last) {
+//         if( nums[first] > nums[last] ) {
+//             res.unshift(nums[first]);
+//             first++;
+//         }else{
+//             res.unshift(nums[last]);
+//             last--;
+//         }
+//     }
+//     return res
+// };
+
 var sortedSquares = function(nums) {
-    for (let i = 0;  i < nums.length; i++) {
-        nums[i] = nums[i] * nums[i];
-    }
     
     let first = 0;
     let last = nums.length - 1;
     let res = [];
     while (first <= last) {
-        if( nums[first] > nums[last] ) {
-            res.unshift(nums[first]);
+        if( nums[first]*nums[first] > nums[last]*nums[last] ) {
+            res.unshift(nums[first]*nums[first]);
             first++;
         }else{
-            res.unshift(nums[last]);
+            res.unshift(nums[last]*nums[last]);
             last--;
         }
     }
