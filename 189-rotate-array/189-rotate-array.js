2e0aa12a770fa3b5a52d2cf3d85df14af14d57b8
Evie Zeng <93158200+evieeee123@users.noreply.github.com>
2022-08-19 17:06:11 -0700

    Time: 133 ms (62.84%), Space: 52.4 MB (45.28%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+// var rotate = function(nums, k) {
+//     while (k > 0){
+//         nums.unshift(nums.pop())
+//         k--;
+//     }
+// };
+
+var rotate = function(nums, k) {
+    k = k % nums.length;
+    
+    nums.reverse();
+    reversed(nums, 0, k - 1);
+    reversed(nums, k, nums.length-1)
+};
+
+const reversed = (nums, start, end) => {
+    while (start <= end) {
+        [nums[start], nums[end]] = [nums[end], nums[start]];
+        start++;
+        end--;
+    }
+}
