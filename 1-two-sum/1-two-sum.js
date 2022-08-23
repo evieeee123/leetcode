5e0cd542b5932e0d7914214a625ede136c002d69
Evie Zeng <93158200+evieeee123@users.noreply.github.com>
2022-08-23 12:50:36 -0700

    Time: 133 ms (47.02%), Space: 43.1 MB (32.00%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,13 +4,20 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    // let res = [];
-    for(let i = 0; i < nums.length; i++) {
-        for(let j = i + 1; j < nums.length; j++) {
-            if(nums[i] + nums[j] === target) {
-                return [i, j]
-            }
-        }
+    // for(let i = 0; i < nums.length; i++) {
+    //     for(let j = i + 1; j < nums.length; j++) {
+    //         if(nums[i] + nums[j] === target) {
+    //             return [i, j]
+    //         }
+    //     }
+    // }
+    let dictionary = {}
+    
+    for (let i = 0; i < nums.length; i++) {
+        const diff = target - nums[i]
+        if (dictionary[nums[i]] !== undefined) 
+            return [dictionary[nums[i]], i]
+        dictionary[diff] = i
     }
 };
 
