e3e03707e0229211b46dbe74de40a2ee6198a026
Evie Zeng <93158200+evieeee123@users.noreply.github.com>
2022-08-22 18:13:16 -0700

    Time: 109 ms (56.41%), Space: 48.7 MB (39.66%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.js
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.js
@@ -2,23 +2,40 @@
  * @param {string} s
  * @return {string}
  */
-var reverseWords = function(s) {
-    let arr = s.split(" ");
-    for(let i = 0; i < arr.length; i++){
-        arr[i] = reversed(arr[i])
-    }
-    return arr.join(" ")
-};
+// var reverseWords = function(s) {
+//     let arr = s.split(" ");
+//     for(let i = 0; i < arr.length; i++){
+//         arr[i] = reversed(arr[i])
+//     }
+//     return arr.join(" ")
+// };
 
-const reversed = function(word){
-    let wordarr = word.split("")
-    let start = 0;
-    let end = word.length - 1;
+// const reversed = function(word){
+//     let wordarr = word.split("")
+//     let start = 0;
+//     let end = word.length - 1;
     
-    while(start <= end) {
-        [wordarr[start], wordarr[end]] = [wordarr[end], wordarr[start]];
-        start++;
-        end--;
+//     while(start <= end) {
+//         [wordarr[start], wordarr[end]] = [wordarr[end], wordarr[start]];
+//         start++;
+//         end--;
+//     }
+//     return wordarr.join("")
+// }
+
+
+var reverseWords = function(s) {
+    let word = "";
+    let res = "";
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        
+        if (char !== " ") {
+            word = char + word
+        }else{
+            res += word + " "
+            word = ""
+        }
     }
-    return wordarr.join("")
-}
+    return res + word
+};
