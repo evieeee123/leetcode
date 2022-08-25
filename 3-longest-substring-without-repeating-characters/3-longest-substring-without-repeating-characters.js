9a72ba81ff33c039ae57150da4400708409ca9e1
Evie Zeng <93158200+evieeee123@users.noreply.github.com>
2022-08-25 16:52:39 -0700

    Time: 828 ms (10.08%), Space: 48.8 MB (36.99%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -1,32 +1,71 @@
 /**
  * @param {string} s
  * @return {number}
- */
-var lengthOfLongestSubstring = function(s) {
-    let max = 0;
-    let start = 0;
-    let hash = {};
+*/
+// var lengthOfLongestSubstring = function(s) {
+//     let max = 0;
+//     let start = 0;
+//     let hash = {};
     
-    for (let end = 0; end < s.length; end++) {
-        let rChar = s[end];
-        if (hash[rChar]) {
-            hash[rChar]++;
-        }else {
-            hash[rChar] = 1
-        }
+//     for (let end = 0; end < s.length; end++) {
+//         let rChar = s[end];
+//         if (hash[rChar]) {
+//             hash[rChar]++;
+//         }else {
+//             hash[rChar] = 1
+//         }
         
-        while (hash[rChar] > 1) {
-            let lChar = s[start]
+//         while (hash[rChar] > 1) {
+//             let lChar = s[start]
             
-            if(hash[lChar] > 1) {
-                hash[lChar]--;
-            } else {
-                delete hash[lChar]
-            }
-            start++;
-        }
+//             if(hash[lChar] > 1) {
+//                 hash[lChar]--;
+//             } else {
+//                 delete hash[lChar]
+//             }
+//             start++;
+//         }
         
-        max = Math.max(max, (end-start) + 1)
+//         max = Math.max(max, (end-start) + 1)
+//     }
+//     return max
+// };
+
+var lengthOfLongestSubstring = function(s) {
+    
+    let charArr = s.split('')
+    if(!s || s.length === 0) return 0;
+    if(s.length === 1) return 1;
+    
+    let pointerA = 0; //Slow Pointer
+    let pointerB = 0 //Fast Pointer
+    
+    let output = 0;
+    
+    let currentCount = 0;
+    let hasSeen = [];
+    
+    while(pointerB < s.length) {
+        let currentVal = charArr[pointerB];
+        
+        if(!hasSeen.includes(currentVal)){
+            hasSeen.push(currentVal);
+            if(output < hasSeen.length){
+                output = hasSeen.length;
+            }  
+            pointerB++;
+        } else{
+            if(output < hasSeen.length){
+                output = hasSeen.length;
+            }          
+            pointerA++;
+            pointerB = pointerA;
+            hasSeen = [];    
+        }
     }
-    return max
+    
+    if(output === 0 && hasSeen.length){
+        output = hasSeen.length;
+    }
+    return output;
 };
