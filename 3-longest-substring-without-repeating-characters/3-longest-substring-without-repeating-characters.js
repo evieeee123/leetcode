8fa53b575de2bd5d21a27269380a5591b3ae4cdd
Evie Zeng <93158200+evieeee123@users.noreply.github.com>
2022-08-25 16:22:40 -0700

    Time: 130 ms (68.12%), Space: 47.9 MB (58.26%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let max = 0;
+    let start = 0;
+    let hash = {};
+    
+    for (let end = 0; end < s.length; end++) {
+        let rChar = s[end];
+        if (hash[rChar]) {
+            hash[rChar]++;
+        }else {
+            hash[rChar] = 1
+        }
+        
+        while (hash[rChar] > 1) {
+            let lChar = s[start]
+            
+            if(hash[lChar] > 1) {
+                hash[lChar]--;
+            } else {
+                delete hash[lChar]
+            }
+            start++;
+        }
+        
+        max = Math.max(max, (end-start) + 1)
+    }
+    return max
+};
