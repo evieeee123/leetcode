a723756eb62e941d0625eccc11b47373187e1b33
Evie Zeng <93158200+evieeee123@users.noreply.github.com>
2022-08-29 14:22:06 -0700

    Time: 8658 ms (5.01%), Space: 48.3 MB (95.53%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -13,19 +13,26 @@
 //     return false
 // };
 
+// var containsDuplicate = function(nums) {
+//     let hash = {};
+    
+//     for(let i = 0; i < nums.length; i++) {
+//         if(!hash[nums[i]]) {
+//             hash[nums[i]] = 1;
+//         } else {
+//             hash[nums[i]]++;
+//         }
+//     }
+    
+//     for (let key in hash) {
+//         if (hash[key] > 1) return true
+//     }
+//     return false;
+// };
+
 var containsDuplicate = function(nums) {
-    let hash = {};
-    
     for(let i = 0; i < nums.length; i++) {
-        if(!hash[nums[i]]) {
-            hash[nums[i]] = 1;
-        } else {
-            hash[nums[i]]++;
-        }
-    }
-    
-    for (let key in hash) {
-        if (hash[key] > 1) return true
+        if (nums.indexOf(nums[i]) !== nums.lastIndexOf(nums[i])) return true
     }
     return false;
 };
