6056cfe28aa2ef14410a8cd0849a7db7e5b1f250
Evie Zeng <93158200+evieeee123@users.noreply.github.com>
2022-08-29 16:42:01 -0700

    Time: 69 ms (89.92%), Space: 42.5 MB (71.98%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.js
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.js
@@ -10,22 +10,51 @@
  * @param {number} n
  * @return {ListNode}
  */
+// var removeNthFromEnd = function(head, n) {
+//     const dummyHead = new ListNode()
+//     dummyHead.next = head;
+    
+//     let fast = dummyHead;
+//     let slow = dummyHead
+    
+//     for(let i = 1; i < n + 1; i++) {
+//         fast = fast.next
+//     }
+    
+//     while (fast.next) {
+//         fast = fast.next;
+//         slow = slow.next
+//     }
+    
+//     slow.next = slow.next.next;
+//     return dummyHead.next
+// };
+
+
 var removeNthFromEnd = function(head, n) {
-    const dummyHead = new ListNode()
-    dummyHead.next = head;
+	let node = head;
+	let counter = 1; //0
     
-    let fast = dummyHead;
-    let slow = dummyHead
-    
-    for(let i = 1; i < n + 1; i++) {
-        fast = fast.next
+
+	while (node.next !== null) {
+		node = node.next;
+		counter++; //5
+} 
+
+// Counter - n - 1 = (Counter - n).next.next
+    let curr = head;
+    let num = 1; //5
+    while (num <= counter) {
+        if (num === counter - n) { //3
+            curr.next = curr.next.next
+        }else if (counter - n === 0  && num === 1) {
+            head = head.next
+        }else {
+            curr = curr.next
+        }
+        num++;
     }
     
-    while (fast.next) {
-        fast = fast.next;
-        slow = slow.next
-    }
-    
-    slow.next = slow.next.next;
-    return dummyHead.next
-};
+    if (counter === 1 && n === 1) head = null
+    return head
+}
