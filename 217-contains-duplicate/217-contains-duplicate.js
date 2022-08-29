4055fd22f705b5fddbe42f780c67571ad9c24110
Evie Zeng <93158200+evieeee123@users.noreply.github.com>
2022-08-29 14:20:14 -0700

    Time: 222 ms (23.68%), Space: 54.5 MB (5.97%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+// var containsDuplicate = function(nums) {
+//     let newArr = nums.sort();
+    
+//     for (let i = 0; i < newArr.length -1; i++) {
+//         if (newArr[i] === newArr[i+1]) {
+//             return true;
+//         }
+//     }
+//     return false
+// };
+
+var containsDuplicate = function(nums) {
+    let hash = {};
+    
+    for(let i = 0; i < nums.length; i++) {
+        if(!hash[nums[i]]) {
+            hash[nums[i]] = 1;
+        } else {
+            hash[nums[i]]++;
+        }
+    }
+    
+    for (let key in hash) {
+        if (hash[key] > 1) return true
+    }
+    return false;
+};
