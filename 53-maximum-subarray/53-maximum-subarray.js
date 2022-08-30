d80a86ada119b2bdcd91eef15213b8f4c1255069
Evie Zeng <93158200+evieeee123@users.noreply.github.com>
2022-08-29 18:12:53 -0700

    Time: 133 ms (37.20%), Space: 50.3 MB (70.52%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -2,11 +2,26 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var maxSubArray = function(nums) {
+//     let res = nums[0];
+//     for (let i = 1; i < nums.length; i++) {
+//         nums[i] = Math.max(nums[i], nums[i] + nums[i-1]) ;
+//         res = Math.max(res, nums[i])
+//     }
+//     return res
+// };
+
 var maxSubArray = function(nums) {
-    let res = nums[0];
-    for (let i = 1; i < nums.length; i++) {
-        nums[i] = Math.max(nums[i], nums[i] + nums[i-1]) ;
-        res = Math.max(res, nums[i])
+    let max = nums[0];
+    let sum = 0;
+    for(let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        if (sum < 0) {
+            sum = 0;
+        }else if (sum > max) {
+            max = sum;
+        }
+        if(nums[i] > max) max = nums[i];
     }
-    return res
+    return max
 };
