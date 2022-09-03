0a9278ad7f108d7aed42e11e77e25a17dbffd937
Evie Zeng <93158200+evieeee123@users.noreply.github.com>
2022-09-02 18:41:50 -0700

    Time: 80 ms (87.97%), Space: 44.4 MB (52.15%) - LeetHub

--- 268-missing-number/268-missing-number.js
+++ 268-missing-number/268-missing-number.js
@@ -2,13 +2,30 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var missingNumber = function(nums) {
+//     let i = 0;
+//     while (i < nums.length + 1) {
+//         if(nums.includes(i)){
+//             i++
+//         } else {
+//             return i;
+//         }
+//     }
+// };
+
 var missingNumber = function(nums) {
     let i = 0;
-    while (i < nums.length + 1) {
-        if(nums.includes(i)){
-            i++
-        } else {
-            return i;
-        }
+    let j = 0;
+    let sumNums = 0;
+    let sum = 0;
+    while(i < nums.length) {
+        sumNums += nums[i]
+        i++;
     }
+    
+    while (j <= nums.length){
+        sum += j;
+        j++;
+    }
+    return sum - sumNums
 };
