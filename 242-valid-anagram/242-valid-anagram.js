ddb6751bed628a976f26cfd040b1d2f0fd32e3f0
Evie Zeng <93158200+evieeee123@users.noreply.github.com>
2022-09-07 21:53:11 -0700

    Time: 137 ms (38.40%), Space: 43 MB (82.82%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -3,29 +3,52 @@
  * @param {string} t
  * @return {boolean}
  */
+// var isAnagram = function(s, t) {
+//     let hash1 = {}
+//     if (s.length !== t.length) return false;
+//     for(let i = 0; i < s.length; i++) {
+//         if(!hash1[s[i]]) {
+//             hash1[s[i]] = 1;
+//         } else {
+//             hash1[s[i]]++;
+//         }
+//     }
+    
+//     for(let i = 0; i < t.length; i++) {
+//         if(!hash1[t[i]]) {
+//             return false;
+//         } else {
+//             hash1[t[i]]--;
+//         }
+//     }
+    
+//     for(let key in hash1){
+//         if(hash1[key] !== 0){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
 var isAnagram = function(s, t) {
-    let hash1 = {}
+    let hash = {};
     if (s.length !== t.length) return false;
-    for(let i = 0; i < s.length; i++) {
-        if(!hash1[s[i]]) {
-            hash1[s[i]] = 1;
+    for(let i = 0; i < s.length; i++){
+        if(!hash[s[i]]){
+            hash[s[i]] = 1;
         } else {
-            hash1[s[i]]++;
+            hash[s[i]] += 1;
         }
     }
-    
-    for(let i = 0; i < t.length; i++) {
-        if(!hash1[t[i]]) {
+    for(let i = 0; i < t.length; i++){
+        if(hash[t[i]]){
+            hash[t[i]] -= 1;
+        }else{
             return false;
-        } else {
-            hash1[t[i]]--;
         }
     }
-    
-    for(let key in hash1){
-        if(hash1[key] !== 0){
-            return false;
-        }
+    for(let key in hash){
+        if (hash[key] !== 0) return false;
     }
     return true;
 }
