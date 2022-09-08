279e569227dcb7491982c39034f2514956c3ab80
Evie Zeng <93158200+evieeee123@users.noreply.github.com>
2022-09-07 22:38:11 -0700

    Time: 120 ms (39.57%), Space: 43.9 MB (94.86%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -2,19 +2,35 @@
  * @param {string} s
  * @return {boolean}
  */
+// var isPalindrome = function(s) {
+//     let str = "";
+//     let char = "abcdefghijklmnopqrstuvwxyz";
+//     let num = "0123456789"
+    
+//     for(let ch of s){
+//         if (char.includes(ch.toLowerCase())) {
+//            str += ch.toLowerCase();
+//         } else if (num.includes(ch)){
+//             str += ch;
+//         }
+//         // console.log(str)
+//     }
+    
+//     return str === str.split("").reverse().join("")
+// };
+
 var isPalindrome = function(s) {
-    let str = "";
-    let char = "abcdefghijklmnopqrstuvwxyz";
-    let num = "0123456789"
+    let newStr = s.replace(/[^a-z0-9]/gi, "").toLowerCase();
+    let i = 0;
+    let j = newStr.length - 1;
     
-    for(let ch of s){
-        if (char.includes(ch.toLowerCase())) {
-           str += ch.toLowerCase();
-        } else if (num.includes(ch)){
-            str += ch;
+    while (i < j){
+        if(newStr[i] === newStr[j]){
+            i++;
+            j--;
+        }else{
+            return false
         }
-        // console.log(str)
     }
-    
-    return str === str.split("").reverse().join("")
+    return true
 };
