076d6d39169aacb3dff9ebc047768d454b855d07
Evie Zeng <93158200+evieeee123@users.noreply.github.com>
2022-09-15 20:04:08 -0700

    Time: 98 ms (38.94%), Space: 42.2 MB (41.07%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let last = m + n - 1;
+    let i = m - 1;
+    let j = n - 1;
+    while (i >= 0 && j >=0 && last >= 0){
+        if (nums1[i] > nums2[j]){
+            nums1[last] = nums1[i]
+            i--;
+        }else{
+            nums1[last] = nums2[j]
+            j--;
+        }
+        last--;
+    }
+    
+     while(i >= 0){
+         nums1[last] = nums1[i];
+         last--;
+         i--;
+     }
+    while(j >= 0){
+         nums1[last] = nums2[j];
+         last--;
+         j--;
+     }
+            
+};
