6c03187e746841656fc6e107b8cef77f6aad46cf
Evie Zeng <93158200+evieeee123@users.noreply.github.com>
2022-09-20 18:39:30 -0700

    Time: 110 ms (11.58%), Space: 42.2 MB (18.69%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -3,7 +3,9 @@
  * @return {number}
  */
 var climbStairs = function(n, memo = {}) {
-    if (n <= 3) return n;
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    if (n === 2) return 2;
     if (n in memo) return memo[n]
     memo[n] = climbStairs(n-1, memo) + climbStairs(n-2, memo)
     return memo[n]
