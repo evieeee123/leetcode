ff421759b1b4d2169f71f335a3c2a7dfe495f14d
Evie Zeng <93158200+evieeee123@users.noreply.github.com>
2022-09-23 19:26:27 -0700

    Time: 80 ms (89.43%), Space: 47.3 MB (28.25%) - LeetHub

--- 136-single-number/136-single-number.js
+++ 136-single-number/136-single-number.js
@@ -2,13 +2,27 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var singleNumber = function(nums) {
+//     let hash = {}
+//     for (let num of nums) {
+//         if(!hash[num]){
+//             hash[num] = 1
+//         }else{
+//             hash[num]++
+//         }
+//     }
+//     for(let key in hash){
+//         if(hash[key] === 1) return key
+//     }
+// };
+
 var singleNumber = function(nums) {
     let hash = {}
-    for (let num of nums) {
-        if(!hash[num]){
-            hash[num] = 1
+    for (let i = 0; i < nums.length; i++) {
+        if(!hash[nums[i]]){
+            hash[nums[i]] = 1
         }else{
-            hash[num]++
+            hash[nums[i]]++
         }
     }
     for(let key in hash){
